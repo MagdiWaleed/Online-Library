03a039609ad67cdc2cade2754400a9ca178cf10c
MagdiWaleed <mmmaaagggdddiii@gmail.com>
2024-04-18 16:32:51 +0200

    make the card in main screen dynamic

--- admin/main/main_controller.js
+++ admin/main/main_controller.js
@@ -1,9 +1,33 @@
-    //     <div class="book_details" onclick="navigateToPage('/admin/single_book/single_book.html')">
-    //     <img src="/references/images/book.jpg" alt="IMAGE-NOT-FOUND" class="book_image" >
-    //     <h4>The Art Of</h4>
-    //     <p>By: Author Name</p>
-    //   </div>
-var bookCard="  <div class='book_details' onclick=''><img src='/references/images/book.jpg' alt='IMAGE-NOT-FOUND' class='book_image' ><h4>The Art Of</h4><p>By: Author Name</p></div>";
+class SingleBookModel{
+
+  constructor(id="#",book_name="#",book_description="#",author_id="#",author_name="#",about_author="#",category="#",image="#"){
+  // this.short_id=GLOBAL_ID;
+  this.id=id;
+  this.book_name=book_name;
+  this.book_description=book_description;
+  this.author_name=author_name;
+  this.author_id=author_id
+  this.about_author=about_author;
+  this.category=category;    
+  this.image =image;
+  }
+}
+
+
+   
+//     <div class="book_details" onclick="navigateToPage('/admin/single_book/single_book.html')">
+//     <img src="/references/images/book.jpg" alt="IMAGE-NOT-FOUND" class="book_image" >
+//     <h4>The Art Of</h4>
+//     <p>By: Author Name</p>
+//   </div>
+// var bookCard="  <div class='book_details' onclick=''><img src='/references/images/book.jpg' alt='IMAGE-NOT-FOUND' class='book_image' ><h4>The Art Of</h4><p>By: Author Name</p></div>";
+
+function buildBookCard(singleBookData){
+  image_layer="<div class='book_details' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><img src='"+singleBookData.image+"' alt='IMAGE-NOT-FOUND' class='book_image' >";
+  book_details_layer="<h4>"+singleBookData.book_name+"</h4><p>By: "+singleBookData.author_name+"</p></div>"
+  return image_layer+book_details_layer;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
 /* <div class="borrowed_books">
@@ -14,38 +38,61 @@ var bookCard="  <div class='book_details' onclick=''><img src='/references/image
 </div >
   <h2 class="BORROWED">BORROWED</h2>
 </div>
-<div class="book_details" onclick="navigateToPage('/admin/single_book/single_book.html')">
- <img src="/references/images/book.jpg" alt="IMAGE-NOT-FOUND" class="book_image" >
- <h4>The Art Of</h4>
- <p>By: Author Name</p>
-</div> */
-var brrowedBook="<div class='borrowed_books'><div style='position: relative;'><img src='/references/images/book.jpg' alt='IMAGE-NOT-FOUND' class='book_image'><h4>Book Title</h4> <p>By: Author Name</p></div ><h2 class='BORROWED'>BORROWED</h2></div><div class='book_details'><img src='/references/images/book.jpg' alt='IMAGE-NOT-FOUND' class='book_image' ><h4>The Art Of</h4><p>By: Author Name</p></div> "
+*/
+// var brrowedBook=" <div class='borrowed_books'><div style='position: relative;'> <img src='/references/images/book.jpg'alt='IMAGE-NOT-FOUND' class='book_image'/><h4>The Art Of</h4><p>By: Author Name</p></div ><h2 class='BORROWED'>BORROWED</h2></div>"
 
+function buildBrrowedBookCard(singleBookData){
+ first_layer="<div class='borrowed_books' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><div style='position: relative;' >";
+ image_layer="<img src='"+singleBookData.image+"'alt='IMAGE-NOT-FOUND' class='book_image'/>";
+ book_details_layer="<h4>"+singleBookData.book_name+"</h4>"+"<p>By:"+singleBookData.author_name+"</p></div ><h2 class='BORROWED'>BORROWED</h2></div>";
+return first_layer+image_layer+book_details_layer;
+}
 
 function getTrendingBooksList  (numberOfElements){
-
-          bookCardsList="";
+         bookCardsList="";
+         console.log(buildBookCard(new SingleBookModel()));
         for(i=0;i<numberOfElements;i++){
+          s =new SingleBookModel();
+          s.id=0;
+          s.book_name= "how to treat poeple well and fuck them in the conroer";
+          s.book_description="its a nice book";
+          s.author_id=11;
+          s.author_name="magdi waleed mohamed khalaf allah";
+          s.category="action";
+          s.about_author="he is good autoher"
+          s.image="/references/images/book.jpg";
+          
             if(i%5==0){
-                bookCardsList+=" "+brrowedBook;
+                bookCardsList+=" "+buildBrrowedBookCard(s);
             }
             else{
-            bookCardsList+=" "+bookCard;
+            bookCardsList+=" "+buildBookCard(s);
             }
+            delete s;
         }
         document.write(bookCardsList);
 }
 
 function getLatestBooksList  (numberOfElements){
-
-    bookCardsList="";
+  bookCardsList="";
   for(i=0;i<numberOfElements;i++){
+    s =new SingleBookModel();
+    s.id=0;
+    s.book_name= "how to treat poeple well and fuck them in the conroer";
+    s.book_description="its a nice book";
+    s.author_id=11;
+    s.author_name="magdi waleed mohamed khalaf allah";
+    s.category="action";
+    s.about_author="he is good autoher"
+    s.image="/references/images/book.jpg";
+    
       if(i%5==0){
-          bookCardsList+=" "+brrowedBook;
+          bookCardsList+=" "+buildBrrowedBookCard(s);
       }
       else{
-      bookCardsList+=" "+bookCard;
+      bookCardsList+=" "+buildBookCard(s);
       }
+      delete s;
   }
   document.write(bookCardsList);
 }
