6c7329537e228f115527ad0464e23c2d2a82acd9
MagdiWaleed <mmmaaagggdddiii@gmail.com>
2024-04-18 21:28:49 +0200

    simple fix to the over flow problem that faced us in books/bookdetails classs

--- admin/main/main_controller.js
+++ admin/main/main_controller.js
@@ -23,25 +23,8 @@ class SingleBookModel{
 // var bookCard="  <div class='book_details' onclick=''><img src='/references/images/book.jpg' alt='IMAGE-NOT-FOUND' class='book_image' ><h4>The Art Of</h4><p>By: Author Name</p></div>";
 
 function buildBookCard(singleBookData){
-  if ((singleBookData.book_name).length > 20) {
-  image_layer="<div class='book_details' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><img src='"+singleBookData.image+"' alt='IMAGE-NOT-FOUND' class='book_image' >";
-  book_details_layer="<h4 class='overflow'>"+singleBookData.book_name+"</h4><p>By: "+singleBookData.author_name+"</p></div>"
-  if ((singleBookData.author_name).length > 17) {
-    console.log("ana khol");
-    image_layer="<div class='book_details' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><img src='"+singleBookData.image+"' alt='IMAGE-NOT-FOUND' class='book_image' >";
-    book_details_layer="<h4 class='overflow'>"+singleBookData.book_name+"</h4><p class='overflow'>By: "+singleBookData.author_name+"</p></div>"
-  }
-}
-     
-  else if ((singleBookData.author_name).length > 17) {
-    console.log("i'm here author");
-    image_layer="<div class='book_details' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><img src='"+singleBookData.image+"' alt='IMAGE-NOT-FOUND' class='book_image' >";
-    book_details_layer="<h4>"+singleBookData.book_name+"</h4><p class='overflow'>By: "+singleBookData.author_name+"</p></div>"
-  }
-  else {
-    image_layer="<div class='book_details' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><img src='"+singleBookData.image+"' alt='IMAGE-NOT-FOUND' class='book_image' >";
-    book_details_layer="<h4>"+singleBookData.book_name+"</h4><p>By: "+singleBookData.author_name+"</p></div>"
-  }
+  image_layer="<div class='book_details ' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><img src='"+singleBookData.image+"' alt='IMAGE-NOT-FOUND' class='book_image' >";
+  book_details_layer="<h4>"+singleBookData.book_name+"</h4><p>By: "+singleBookData.author_name+"</p></div>"
   return image_layer+book_details_layer;
 }
 
@@ -58,7 +41,6 @@ function buildBookCard(singleBookData){
 */
 // var brrowedBook=" <div class='borrowed_books'><div style='position: relative;'> <img src='/references/images/book.jpg'alt='IMAGE-NOT-FOUND' class='book_image'/><h4>The Art Of</h4><p>By: Author Name</p></div ><h2 class='BORROWED'>BORROWED</h2></div>"
 
-
 function buildBrrowedBookCard(singleBookData){
  first_layer="<div class='borrowed_books' onclick=\"navigateToPage('/admin/single_book/single_book.html')\"><div style='position: relative;' >";
  image_layer="<img src='"+singleBookData.image+"'alt='IMAGE-NOT-FOUND' class='book_image'/>";
@@ -71,10 +53,10 @@ function getTrendingBooksList  (numberOfElements){
         for(i=0;i<numberOfElements;i++){
           s =new SingleBookModel();
           s.id=0;
-          s.book_name= "how to type to type ez like a bitch";
-          s.book_description="how t";
+          s.book_name= "how to treat poeple well and fuck them in the conroer";
+          s.book_description="its a nice book";
           s.author_id=11;
-          s.author_name="magdi waleed khalf allah";
+          s.author_name="magdi waleed mohamed khalaf allah";
           s.category="action";
           s.about_author="he is good autoher"
           s.image="/references/images/book.jpg";
@@ -95,10 +77,10 @@ function getLatestBooksList  (numberOfElements){
   for(i=0;i<numberOfElements;i++){
     s =new SingleBookModel();
     s.id=0;
-    s.book_name= "how ";
+    s.book_name= "how to treat poeple well and fuck them in the conroer";
     s.book_description="its a nice book";
     s.author_id=11;
-    s.author_name="Ahmed mohammed";
+    s.author_name="magdi waleed mohamed khalaf allah";
     s.category="action";
     s.about_author="he is good autoher"
     s.image="/references/images/book.jpg";
