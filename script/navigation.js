2d6f0958d751abe9c7127d39cb3e3b06401d52f8
MagdiWaleed <mmmaaagggdddiii@gmail.com>
2024-04-21 01:50:17 +0200

    done

--- script/navigation.js
+++ script/navigation.js
@@ -7,24 +7,24 @@ function showpopup(){
         const storedUserDataString = localStorage.getItem("user");
         const storedUserData = JSON.parse(storedUserDataString);
         console.log(storedUserData.check);
-        if(storedUserData.check == false){
+        if( sessionStorage.getItem("user_mode")=="enable"){
+            navigateToPage("/user/my_information/my_information.html");
+            const userData = {
+                "name": 'admin',
+                "pass": 'AN ADMIN',
+                "confpass": 'AN ADMIN',
+                "mail": "admin@admin.com",
+                "check": "admin"
+            };
+            const userDataString = JSON.stringify(userData);
+            localStorage.setItem("user", userDataString);
+        }
+        else if(storedUserData.check == false){
         
         navigateToPage('/user/my_information/my_information.html');
-    }else if(sessionStorage.getItem("user_mode")=="enable"){
-        navigateToPage("/user/my_information/my_information.html");
-        const userData = {
-            "name": 'admin',
-            "pass": 'AN ADMIN',
-            "confpass": 'AN ADMIN',
-            "mail": "admin@admin.com",
-            "check": "admin"
-        };
-        const userDataString = JSON.stringify(userData);
-        localStorage.setItem("user", userDataString);
-    
-
     }
     }
+     
     else{
     let pop_up = document.getElementById("popup");
     pop_up.classList.add("open-popup");}
